36bf595bc202edf4ab2c6aa382b3bc58fc74b081
Guillaume AZAM <guillaumeazam@MacBook-Pro-de-TALAN.local>
2023-01-10 13:47:25 +0100

    feat: hidde solution

--- haskell/src/Triangle.hs
+++ haskell/src/Triangle.hs
@@ -1,6 +1,4 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows n = take n pascal where
-  pascal = [1] : map next pascal
-  next r = zipWith (+) (0:r) $ r++[0]
+rows  n = error "You need to implement this function."
