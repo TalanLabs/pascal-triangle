e4cac3cc5a5774f260901ee385ecec34248f8eea
Guillaume AZAM <guillaumeazam@MacBook-Pro-de-TALAN.local>
2023-08-21 16:29:11 +0200

    feat(javascript): add javascript template

---
+++ javascript/pascals-triangle.js
@@ -0,0 +1,10 @@
+/**
+* This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
+* convenience to get you started writing code faster.
+*
+* @param {number} level The given level to generate.
+* @return {number[][]} The array containing the pascals triangle.
+*/
+export const rows = (level) => {
+
+}
